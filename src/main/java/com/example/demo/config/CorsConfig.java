819fe8fe7f55b4b3649499c67a6637c8d1ec69ef
Admin <vinhpirlo2002@gmail.com>
2024-02-26 09:07:57 +0700

    Update Cors

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/*")
+        registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000") 	//Da chuyen sang IP FE
                 .allowedMethods("GET", "POST", "PUT", "DELETE"); // Allow specific HTTP methods
     }
