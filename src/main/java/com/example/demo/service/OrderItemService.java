cc72b7280e39fa265aaedf040df657a486ad632f
Admin <vinhpirlo2002@gmail.com>
2024-03-01 09:06:56 +0700

    Update

--- src/main/java/com/example/demo/service/OrderItemService.java
+++ src/main/java/com/example/demo/service/OrderItemService.java
@@ -4,6 +4,7 @@ import com.example.demo.model.OrderItem;
 import com.example.demo.repository.OrderItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,7 +14,7 @@ public class OrderItemService {
 
     @Autowired
     private OrderItemRepository orderItemRepository;
-
+    @Transactional
     public List<OrderItem> getAllOrderItems() {
 
         return orderItemRepository.findAll();
