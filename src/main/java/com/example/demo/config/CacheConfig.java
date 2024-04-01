345919b4ff65637a212fb6517392d081718b0ebb
vinhnhp <vinhnhp@vnpt.vn>
2024-04-01 14:02:59 +0700

    Cap nhat thong ke doanh thu theo giai doan tu ngay - ngay

--- src/main/java/com/example/demo/config/CacheConfig.java
+++ src/main/java/com/example/demo/config/CacheConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -16,16 +17,20 @@ public class CacheConfig {
     @Bean
     public Caffeine<Object, Object> caffeineConfig() {
         return Caffeine.newBuilder()
-                .expireAfterWrite(240, TimeUnit.MINUTES) // Thời gian sống của cache: 240 phút (4 giờ)
+                .expireAfterWrite(240, TimeUnit.MINUTES)
                 .maximumSize(100);
     }
 
     @Bean
-    public CaffeineCacheManager cacheManager() {
+    @Primary
+    public CaffeineCacheManager productCacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();
         cacheManager.setCaffeine(caffeineConfig());
+        cacheManager.setCacheNames(List.of("products", "tables"));
 
-        cacheManager.setCacheNames(List.of("products"));
+//        cacheManager.setCacheNames(List.of("products"));
         return cacheManager;
     }
+
+
 }
--- src/main/java/com/example/demo/service/BanService.java
+++ src/main/java/com/example/demo/service/BanService.java
@@ -1,9 +1,10 @@
-
 package com.example.demo.service;
 
 import com.example.demo.model.Ban;
 import com.example.demo.repository.BanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class BanService {
         this.tableRepository = tableRepository;
     }
 
+    @Cacheable("tables")
     public List<Ban> getAllTables() {
         return tableRepository.findAll();
     }
@@ -26,11 +28,12 @@ public class BanService {
     public Optional<Ban> getTableById(Long tableId) {
         return tableRepository.findById(tableId);
     }
-    
+
     public Optional<Ban> getTableByStatus(boolean statusTable){
-    	return tableRepository.findByStatus(statusTable);
+        return tableRepository.findByStatus(statusTable);
     }
 
+    @CacheEvict(value = "tables", allEntries = true)
     public Ban saveTable(Ban table) {
         return tableRepository.save(table);
     }
@@ -48,6 +51,7 @@ public class BanService {
         }
     }
 
+    @CacheEvict(value = "tables", key = "#result.id")
     public void deleteTable(Long tableId) {
         tableRepository.deleteById(tableId);
     }
