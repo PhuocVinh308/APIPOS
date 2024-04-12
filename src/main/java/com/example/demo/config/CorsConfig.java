946519e0f150c0913b9a54d1f0538192abe57adb
Phước Vinh <vinhpirlo2002@gmail.com>
2024-04-12 20:34:28 +0700

    cors

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -8,11 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableScheduling
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/auth/addNewUser").allowedOrigins("*").allowedMethods("GET", "POST");
+        registry.addMapping("/auth/Token").allowedOrigins("*").allowedMethods("GET", "POST");
+        registry.addMapping("/api/products/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
+        registry.addMapping("/api/table/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
+        registry.addMapping("/api/customers/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
+        registry.addMapping("/api/orders/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
+        registry.addMapping("/api/order-items/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
+        registry.addMapping("/api/employees/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
     }
 }
