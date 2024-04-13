b5ef626dd72d28eaa35bf2be356531197a65ba4a
truongvinh1612 <truongvinh1612@gmail.com>
2024-04-13 10:45:53 +0700

    chinh cors

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -1,22 +1,27 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableScheduling
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/auth/addNewUser").allowedOrigins("*").allowedMethods("GET", "POST");
-        registry.addMapping("/auth/Token").allowedOrigins("*").allowedMethods("GET", "POST");
-        registry.addMapping("/api/products/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
-        registry.addMapping("/api/table/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
-        registry.addMapping("/api/customers/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
-        registry.addMapping("/api/orders/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
-        registry.addMapping("/api/order-items/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
-        registry.addMapping("/api/employees/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Authorization");
+        // Cấu hình CORS cho các yêu cầu liên quan đến `auth`
+        registry.addMapping("/auth/**")
+                .allowedOrigins("http://localhost:3000") // Chỉ cho phép từ nguồn cụ thể
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true) // Cho phép sử dụng chứng thực (credentials)
+                .maxAge(3600);
+
+        // Cấu hình CORS cho các yêu cầu liên quan đến `api`
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000") // Chỉ cho phép từ nguồn cụ thể
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type") // Chỉ định các tiêu đề HTTP được phép
+                .allowCredentials(true) // Cho phép sử dụng chứng thực (credentials)
+                .maxAge(3600);
     }
 }
