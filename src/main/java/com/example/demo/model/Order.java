00a84ea1ac8597fc721e7b224de4bc0c36e81436
Phước Vinh <vinhpirlo2002@gmail.com>
2024-04-16 20:27:57 +0700

    Add thong tin nhan vien order

--- src/main/java/com/example/demo/model/Order.java
+++ src/main/java/com/example/demo/model/Order.java
@@ -12,13 +12,9 @@ import java.util.List;
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-
     private Long id;
-
     @ManyToOne
     private Ban ban;
-
     @JsonIgnore
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems;
@@ -32,61 +28,7 @@ public class Order {
 
     @Column(name = "phone_number")
     private String phoneNumber;
-    public Order() {
-        this.orderDate = new Date();
-    }
-
-    public Order(Long id, Ban ban, List<OrderItem> orderItems, Date orderDate, String phoneNumber) {
-        this.id = id;
-        this.ban = ban;
-        this.orderItems = orderItems;
-        this.orderDate = orderDate;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Ban getBan() {
-        return ban;
-    }
-
-    public void setBan(Ban ban) {
-        this.ban = ban;
-    }
-
-    public List<OrderItem> getOrderItems() {
-        return orderItems;
-    }
-
-    public void setOrderItems(List<OrderItem> orderItems) {
-        this.orderItems = orderItems;
-    }
-
-    public Date getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(Date orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public double getTotalAmount() {
-        return totalAmount;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
+    @OneToOne
+    private Employee employee;
 
 }
