cf7cb657378a8663c857767fcf354a9eadf4f354
vinhnhp <vinhnhp@vnpt.vn>
2024-05-08 17:02:30 +0700

    update username password

--- src/main/java/com/example/demo/utils/jasper/ReportType.java
+++ src/main/java/com/example/demo/utils/jasper/ReportType.java
@@ -1,10 +1,5 @@
 package com.example.demo.utils.jasper;
 
-/**
- * Created by Leemintran on 03/11/21.
- * VNPT-IT KV5
- * toitv.tgg@vnpt.vn
- */
 public enum ReportType {
     PDF, XLS, XLSX, RTF, HTML
 }
